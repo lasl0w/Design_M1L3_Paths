a860470b1a94bdd366b5c3eb0799afcd6fc19690
Tom Montgomery <montgomery3681@gmail.com>
2023-08-28 16:16:10 -0600

    Initial Commit

---
+++ Design_M1L3_Paths/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Design_M1L3_Paths
+//
+//  Created by tom montgomery on 8/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design_M1L3_Paths/Design_M1L3_PathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L3_PathsApp.swift
+//  Design_M1L3_Paths
+//
+//  Created by tom montgomery on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L3_PathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
